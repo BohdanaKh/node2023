a02f59e7287120dfc2db93c22dc0baf916a6658d
BohdanaKh <dananvm@gmail.com>
2023-06-03 22:42:50 +0300

    add homework1 node

--- app.js
+++ app.js
@@ -12,52 +12,57 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.mkdir(path.join(__dirname,'mainDirectory'),(err) => {
+// fs.mkdir(path.join(__dirname,'mainDirectory'),(err) => {
+//     if (err) throw new Error(err.message);
+// })
+//
+//
+// fs.mkdir(path.join(__dirname,'mainDirectory','folder1'),(err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.mkdir(path.join(__dirname,'mainDirectory','folder2'),(err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.mkdir(path.join(__dirname,'mainDirectory','folder3'),(err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.mkdir(path.join(__dirname,'mainDirectory','folder4'),(err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.mkdir(path.join(__dirname,'mainDirectory','folder5'),(err) => {
+//     if (err) throw new Error(err.message);
+// })
+
+
+
+// fs.writeFile(path.join(__dirname,'mainDirectory', 'folder1', 'text1.txt'),'1', (err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.writeFile(path.join(__dirname,'mainDirectory', 'folder2', 'text2.txt'),'2', (err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.writeFile(path.join(__dirname,'mainDirectory', 'folder3', 'text3.txt'),'3', (err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.writeFile(path.join(__dirname,'mainDirectory', 'folder4', 'text4.txt'),'4', (err) => {
+//     if (err) throw new Error(err.message);
+// })
+// fs.writeFile(path.join(__dirname,'mainDirectory', 'folder5', 'text5.txt'),'5', (err) => {
+//     if (err) throw new Error(err.message);
+// })
+
+fs.readdir(path.join(__dirname,'mainDirectory'),(err,data) => {
     if (err) throw new Error(err.message);
+    console.log(data);
 })
 
 
-fs.mkdir(path.join(__dirname,'mainDirectory','folder1'),(err) => {
-    if (err) throw new Error(err.message);
-})
-fs.mkdir(path.join(__dirname,'mainDirectory','folder2'),(err) => {
-    if (err) throw new Error(err.message);
-})
-fs.mkdir(path.join(__dirname,'mainDirectory','folder3'),(err) => {
-    if (err) throw new Error(err.message);
-})
-fs.mkdir(path.join(__dirname,'mainDirectory','folder4'),(err) => {
-    if (err) throw new Error(err.message);
-})
-fs.mkdir(path.join(__dirname,'mainDirectory','folder5'),(err) => {
-    if (err) throw new Error(err.message);
-})
-
-
-
-fs.writeFile(path.join(__dirname,'mainDirectory','text1.txt'),'Hello', (err) => {
-    if (err) throw new Error(err.message);
-})
-fs.writeFile(path.join(__dirname,'mainDirectory','text2.txt'),'Hello', (err) => {
-    if (err) throw new Error(err.message);
-})
-fs.writeFile(path.join(__dirname,'mainDirectory','text3.txt'),'Hello', (err) => {
-    if (err) throw new Error(err.message);
-})
-fs.writeFile(path.join(__dirname,'mainDirectory','text4.txt'),'Hello', (err) => {
-    if (err) throw new Error(err.message);
-})
-fs.writeFile(path.join(__dirname,'mainDirectory','text5.txt'),'Hello', (err) => {
-    if (err) throw new Error(err.message);
-})
-
-
-fs.readdir(path.join(__dirname,'mainDirectory'),{withFileTypes:true}, (err, files) => {
-    if (err) throw new Error(err.message);
-    files.forEach(file => {
-        console.log(file.isFile() ? `FILE: ${file.name}` : `FOLDER: ${file.name}`);
-    })
-})
+// fs.readdir(path.join(__dirname,'mainDirectory'),{withFileTypes:true}, (err, files) => {
+//     if (err) throw new Error(err.message);
+//     files.forEach(file => {
+//         console.log(file.isFile() ? `FILE: ${file.name}` : `FOLDER: ${file.name}`);
+//     })
+// })
 
 
 
